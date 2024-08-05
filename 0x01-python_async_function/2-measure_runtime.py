18322bf1534239030cd796dca6718b9a07cfb988
bravin-onwonga <onbravin@gmail.com>
2024-08-05 23:22:53 +0300

    Using gather to run several coroutines

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Measure the runtime of an asynch program"""
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Function to measure the time taken"""
