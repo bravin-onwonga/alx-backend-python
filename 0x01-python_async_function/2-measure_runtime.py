68b82df115d650451f5e77da0f4bf1b44ae795bd
bravin-onwonga <onbravin@gmail.com>
2024-08-05 23:39:41 +0300

    Calculate the time taken

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 """Measure the runtime of an asynch program"""
 
+import asyncio
+import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
-    """Function to measure the time taken"""
+def measure_time(n: int, max_delay: int) -> float:
+    """Function to measure the time taken for the function
+    imported to finish executing"""
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    time_taken = time.perf_counter() - start
+    return time_taken / n
