4937d4e819811be81d407f284180e9fe6e1313a6
bravin-onwonga <onbravin@gmail.com>
2024-08-06 08:39:57 +0300

    fix:Using type annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,11 @@
 """Execute multiple coroutines at the same time with async"""
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List:
     """Takes two integers n and max_delay and spawns the async function
     wait_random n times and returns a list of all the delays"""
     my_list = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,11 +2,12 @@
 """Similar code to the 1-concurrent_coroutine.wait_n function"""
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int, max_delay: int) -> List:
     """Takes two integers n and max_delay and spawns the async function
     wait_random n times and returns a list of all the delays"""
     my_list = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
