50be8de72cfc4cd7baa19acd821482d54e38de4d
bravin-onwonga <onbravin@gmail.com>
2024-08-09 23:56:24 +0300

    fix:Returning type Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Module on async generator"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Generator function that loops ten times and yields a random
     float between 0 and 10"""
     for _ in range(10):
